b3027bb93f05a5f5d6eda94a8f911ebd7f7c2ef6
M Mantle <mmantlehud@googlemail.com>
2021-11-17 11:15:13 +0000

    Added solution

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 import {setFilms,searchFilms} from './filmModel.js';
-// import {loadData} from './loadData.js';
-// import {validSearchTerm} from './validationFncs.js';
+import {loadData} from './loadData.js';
+import {validSearchTerm} from './validationFncs.js';
 
 
-// let films; //Array of film objects
+
 let filmsList; //<ul id="film-list"></ul>
 let goBtn; // <button id="go-btn">GO</button>
 let searchBox; //<input id="search-box" placeholder="Enter a film title">
@@ -21,15 +21,6 @@ function init(data){
   searchBox.addEventListener("keyup",doSearch);
 }
 
-function loadData(url,callback)
-{
-	fetch(url).then(function(response) {
-		return response.json();
-	}).then(function(data) {
-		callback(data);
-	});
-}
-
 function doSearch()
 {
   clearSearchResults();
@@ -39,24 +30,6 @@ function doSearch()
     displayResults(matchingFilms);
   }
 }
-
-function validSearchTerm(searchTerm){
-  if(searchTerm.length >= 2){
-    return true;
-  }
-  return false;
-}
-
-// function searchFilms(searchTerm){
-//   const matchingFilms = films.filter(function(film){
-//     if(film.title.toLowerCase().search(searchTerm)>-1){
-//       return true;
-//     }else{
-//       return false;
-//     }
-//   })
-//   return matchingFilms;
-// }
 
 function clearSearchResults(){
   while(filmsList.firstChild){
--- js/filmModel.js
+++ js/filmModel.js
@@ -1,21 +1,3 @@
-// const filmModel = {
-//   films:[],
-//   init:function(data)
-//   {
-//   	this.films=data
-//   },
-//   searchFilms:function(searchTerm){
-//     const matchingFilms = this.films.filter(function(film){
-//       if(film.title.toLowerCase().search(searchTerm)>-1){
-//         return true;
-//       }else{
-//         return false;
-//       }
-//     })
-//     return matchingFilms;
-//   }
-// }
-
 let films;
 
 function setFilms(data){
